4a656c5a47f398ecc80570a5b2b7fe30cbfcfe64
owenHochwald <owenhochwald@gmail.com>
2025-03-29 16:58:48 -0700

    add route stubs

---
+++ backend/routes/contactRoutes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/", (req, res) => {
+    res.status(200).json({message: "Get all contacts"});
+});
+
+router.get("/:id", (req, res) => {
+    res.status(200).json({message: `Get contact for ${req.params.id}`});
+});
+
+
+
+router.post("/", (req, res) => {
+    res.status(200).json({message: "Create contacts"});
+});
+
+
+router.put("/:id", (req, res) => {
+    res.status(200).json({message: `Update contact for ${req.params.id}`});
+});
+
+
+router.delete("/:id", (req, res) => {
+    res.status(200).json({message: `Delete contact for ${req.params.id}`});
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use("/api/contacts", require("./routes/contactRoutes"))
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+}); 
+
