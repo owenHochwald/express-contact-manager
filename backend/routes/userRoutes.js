a532aedfb53081a952547ef1e262358efe6b5824
owenHochwald <owenhochwald@gmail.com>
2025-03-30 12:42:38 -0700

    create user routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+    res.json({ message: "Register the user"})
+});
+
+router.post("/login", (req, res) => {
+    res.json({ message: "Login the user"})
+});
+
+
+router.get("/current", (req, res) => {
+    res.json({ message: "Get current user information"})
+});
+
+module.exports = router;
+
+
+
--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ app.use(express.json());
 
 // routes
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 // error handling middleware
 app.use(errorHandler);
