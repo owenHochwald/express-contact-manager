57d67dc57540c61d2683cdfcb86cbfdb6fdd3fa1
owenHochwald <owenhochwald@gmail.com>
2025-03-30 10:54:05 -0700

    add error handling middleware

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -3,6 +3,11 @@
 //@access public
 
 const getContact = (req, res) => {
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
+        res.status(400)
+        throw new Error("All fields are required");
+    }
     res.status(200).json({message: "Get all contacts"})
 }
 
--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,15 @@ const app = express();
 
 const port = process.env.PORT;
 
-app.use("/api/contacts", require("./routes/contactRoutes"))
+// provides a middleware to parse json data
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+// error handling middleware
+app.use(errorHandler);
+
 
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-}); 
+});
 
