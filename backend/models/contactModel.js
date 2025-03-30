37475ab4a6fbada5043633709b84df0fda02e3d7
owenHochwald <owenhochwald@gmail.com>
2025-03-30 12:09:30 -0700

    create contact model

---
+++ backend/models/contactModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const contactSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add a contact name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please add an email address"]
+    },
+    phone: {
+        type: String,
+        required: [true, "Please add a phone number"]
+    },
+    timestamp: true
+});
+
+module.exports = mongoose.model("Contact", contactSchema);
