5f9085a2182d38e28c7b965988ac767ab388e3e0
owenHochwald <owenhochwald@gmail.com>
2025-03-30 13:01:09 -0700

    add user model and implement register user endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,36 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 //@desc Register a new user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Register user"});
+    const { username, email, password} = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are required");
+    }
+    const userAvailable = await User.findOne({ email });
+    if (userAvailable) {
+        res.status(400);
+        throw new Error("User already registered");
+    }
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
+    const user = await User.create({
+        username: username,
+        email: email,
+        password: hashedPassword
+    });
 
+    if (user) {
+        res.status(201).json({id: user._id, email: user.email});
+    } else {
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
 });
 
 //@desc Login an existing user
